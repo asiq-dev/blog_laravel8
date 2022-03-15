17bb665828bc3ce4e052686c7cff4da267a21502
asiqul islam <asiq.dev@gmail.com>
2022-03-15 23:07:17 +0600

    upadetd with post call by slug

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,8 +1,8 @@
 <x-layout>
     @foreach ($posts as $post)
     <article>
-        <h1><a href="/posts/{{ $post->id }}">
-        {{ $post->title }}
+        <h1><a href="/posts/{{ $post->slug }}">
+        {!! $post->title !!}
         </a>
         </h1>
 
