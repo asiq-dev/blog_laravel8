095b589fb72a4ecc2f856a56b59da1002f601b33
asiqul islam <asiq.dev@gmail.com>
2022-04-12 21:31:12 +0600

    search with keywords from title and body and dynamic search using controller and Model filter

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,6 +13,18 @@ class Post extends Model
     protected $with = ['author','category'];
     // protected $fillable = ['title','excerpt','body'];
 
+
+public function scopeFilter($query, array $filters) //this func use for query data and return on controller / Post::newQuery()->filter();
+{
+    $query->when($filters['search'] ?? false, function ($query, $search) {
+        $query->where('title', 'like', '%' .$search . '%')
+        ->orWhere('body', 'like', '%' .$search . '%');
+    });
+}
+
+
+
+
     public function category(){
         return $this->belongsTo(Category::class);
     } 
